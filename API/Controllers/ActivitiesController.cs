f0630bda7052ddfcda95c813b74b99b88551ea8a
Pantoine <yvan.antoine84@gmail.com>
2021-09-21 18:03:25 +0200

    Section 3 - Add MediatR & List activites w/ Query

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,31 +1,33 @@
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Application.Activities;
 using Domain;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 namespace API.Controllers
 {
-    public class ActivitiesController : BaseAPIController
+  public class ActivitiesController : BaseAPIController
+  {
+    private readonly IMediator _mediator;
+    public ActivitiesController(IMediator mediator)
     {
-        private readonly DataContext _context;
-        public ActivitiesController(DataContext context)
-        {
-            _context = context;
-        }
-
-        [HttpGet]
-        public async Task<ActionResult<List<Activity>>> GetActivities()
-        {
-            return await _context.Activities.ToListAsync();
-        }
-
-        [HttpGet("{id}")] // /activites/id
-        public async Task<ActionResult<Activity>> GetActivity(Guid id)
-        {
-            return await _context.Activities.FindAsync(id);
-        }
+        _mediator = mediator;
     }
+
+    [HttpGet]
+    public async Task<ActionResult<List<Activity>>> GetActivities()
+    {
+      return await _mediator.Send(new List.Query());
+    }
+
+    [HttpGet("{id}")] // /activites/id
+    public async Task<ActionResult<Activity>> GetActivity(Guid id)
+    {
+        return Ok();
+    }
+  }
 }
